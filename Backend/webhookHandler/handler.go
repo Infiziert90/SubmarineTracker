ebbc1fafddf5a40b74e838a8a80c4ebf8faa0d16
Infi <infiziert@protonmail.ch>
2025-09-03 22:26:00 +0200

    - Update Item data - Better rate-limit handling for webhooks

--- Backend/webhookHandler/handler.go
+++ Backend/webhookHandler/handler.go
@@ -21,6 +21,13 @@ type ActiveReturn struct {
 	ReturnTime  int64
 }
 
+type RateLimitResponse struct {
+	Message    string  `json:"message"`
+	RetryAfter float64 `json:"retry_after"`
+	Global     bool    `json:"global"`
+	ErrorCode  int     `json:"code,omitempty"`
+}
+
 // HandleReturn - Sleeps until the time has elapsed and prepares the notification.
 func HandleReturn(active ActiveReturn) {
 	var sleepTime = active.ReturnTime - time.Now().Unix()
@@ -51,7 +58,7 @@ func HandleReturn(active ActiveReturn) {
 	SendWebhook(active.WebhookURL, content)
 }
 
-// SendWebhook - Sends the webhook as JSON encoded payload and check if the return value for errors.
+// SendWebhook - Sends the webhook as JSON encoded payload and checks the return value for errors.
 func SendWebhook(webhookUrl string, content WebhookContent) {
 	payload := new(bytes.Buffer)
 	err := json.NewEncoder(payload).Encode(content)
@@ -66,6 +73,23 @@ func SendWebhook(webhookUrl string, content WebhookContent) {
 		return
 	}
 
+	// Rate-Limit hit, retry after waiting
+	if resp.StatusCode == 429 {
+		fmt.Println("Rate limit exceeded")
+		var rateLimit RateLimitResponse
+		err = json.NewDecoder(resp.Body).Decode(&rateLimit)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to convert response to rate-limit json: %v\n", err)
+			return
+		}
+
+		time.Sleep(time.Duration(rateLimit.RetryAfter * float64(time.Second)))
+		SendWebhook(webhookUrl, content)
+
+		return
+	}
+
 	if resp.StatusCode != 200 && resp.StatusCode != 204 {
 		defer resp.Body.Close()
 
@@ -75,7 +99,7 @@ func SendWebhook(webhookUrl string, content WebhookContent) {
 			return
 		}
 
-		fmt.Fprintf(os.Stderr, "Error response was: %v\n", fmt.Errorf(string(responseBody)))
+		fmt.Fprintf(os.Stderr, "Error response was: %s\n", string(responseBody))
 		return
 	}
 }
